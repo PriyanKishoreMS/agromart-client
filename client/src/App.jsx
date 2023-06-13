c96a1692935536cbc8c776c71e5744baf3f7d892
PriyanKishoreMS <priyankishore2000@gmail.com>
2023-06-14 02:36:21 +0530

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+function App() {
+	return (
+		<>
+			{/* <Home /> */}
+			<Login />
+		</>
+	);
+}
+
+export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+	return (
+		<>
+			<Navbar />
+			<div className='p-5 m-5'>
+				<h1 className='text-lg'>This is the homepage</h1>
+			</div>
+		</>
+	);
+};
+
+export default Home;
