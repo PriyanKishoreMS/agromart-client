81aee0eef415cd405984fd75cd393ce4d26de130
PriyanKishoreMS <priyankishore2000@gmail.com>
2023-08-10 18:43:07 +0530

    env

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,14 +1,21 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-const {
-	VITE_API_KEY,
-	VITE_AUTH_DOMAIN,
-	VITE_PROJECT_ID,
-	VITE_STORAGE_BUCKET,
-	VITE_MESSAGING_SENDER_ID,
-	VITE_APP_ID,
-} = import.meta.env;
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+// const {
+// 	VITE_API_KEY,
+// 	VITE_AUTH_DOMAIN,
+// 	VITE_PROJECT_ID,
+// 	VITE_STORAGE_BUCKET,
+// 	VITE_MESSAGING_SENDER_ID,
+// 	VITE_APP_ID,
+// } = import.meta.env;
+
+const VITE_API_KEY = "AIzaSyD2GivdEALQTOoeAvcP9B58Kr7manSezP8";
+const VITE_AUTH_DOMAIN = "agromart-bbf0c.firebaseapp.com";
+const VITE_PROJECT_ID = "agromart-bbf0c";
+const VITE_STORAGE_BUCKET = "agromart-bbf0c.appspot.com";
+const VITE_MESSAGING_SENDER_ID = "835063498215";
+const VITE_APP_ID = "1:835063498215:web:e8d952b5899289f828e942";
 
 const firebaseConfig = {
 	apiKey: VITE_API_KEY,
@@ -19,9 +26,8 @@ const firebaseConfig = {
 	appId: VITE_APP_ID,
 };
 
-
 if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+	firebase.initializeApp(firebaseConfig);
 }
 
 export default firebase;
